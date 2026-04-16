3757b4ccb5d4fafbfcf898181f6e5a48f1e20230
SergioRCarvalho <sergio_pvz3@hotmail.com>
2026-04-16 15:41:42 +0100

    feat scroll to ajuda forms

--- src/components/admin/PublicationFeedbackButton.tsx
+++ src/components/admin/PublicationFeedbackButton.tsx
@@ -2,7 +2,7 @@
 
 import { Button } from "@ama-pt/agora-design-system";
 
-const SUPPORT_PAGE_PATH = "/pages/support";
+const SUPPORT_PAGE_PATH = "/pages/support#ajuda";
 
 export default function PublicationFeedbackButton() {
   return (
