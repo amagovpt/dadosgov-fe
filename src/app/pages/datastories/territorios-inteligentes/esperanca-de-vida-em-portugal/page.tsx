c6c2ed3870df82bf2248a430ff45b38423f7c039
Vítor Almeida <vitor13almeida@gmail.com>
2026-05-04 17:48:44 +0100

    Refactor datastory pages to implement dynamic metadata generation

--- src/app/pages/datastories/territorios-inteligentes/esperanca-de-vida-em-portugal/page.tsx
+++ src/app/pages/datastories/territorios-inteligentes/esperanca-de-vida-em-portugal/page.tsx
@@ -1,13 +1,32 @@
 import { Metadata } from "next";
-import { getDatastory } from "@/queries/datastories/datastory";
+import { getDatastory, getDatastoryMetadata } from "@/queries/datastories/datastory";
 import DatastoryDetails from "@/components/datastories/DatastoryDetails";
 import { BreadcrumbItem } from "@/types/shared";
 
-export const metadata: Metadata = {
-  title: "Data Story - dados.gov.pt",
-};
+export async function generateMetadata({
+  //params,
+}: {
+  params: Promise<{ locale: string }>;
+}): Promise<Metadata> {
+  const datastory = await getDatastoryMetadata(
+    "territorios-inteligentes/esperanca-de-vida-em-portugal",
+    "pt"
+  );
 
-export default async function DataStoryDetailPage() {
+  return {
+    title: datastory.title,
+    description: datastory.description,
+    openGraph: {
+      images: datastory.image.map((i)=> i.url),
+    },
+  };
+}
+
+export default async function DataStoryDetailPage({
+  //params,
+}: {
+  params: Promise<{ locale: string }>;
+}) {
   const breadcrumbItems: BreadcrumbItem[] = [
     { label: "Início", url: "/" },
     { label: "Datastories", url: "/pages/datastories" },
