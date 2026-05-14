65f010789cc761469cdc13fd712ec851c3751b76
Vítor Almeida <vitor13almeida@gmail.com>
2026-05-14 18:33:19 +0100

    refactor: improve formatting of description in HeroCourses component

--- src/components/Learn/Hero/index.tsx
+++ src/components/Learn/Hero/index.tsx
@@ -3,55 +3,59 @@ import Image from "next/image";
 import { Breadcrumb } from "@ama-pt/agora-design-system";
 import dayjs from "dayjs";
 import { parseHtmlToParagraphs } from "@/utils/htmlToParagraphs";
-
+import { formatHtmlParagraphs } from "@/utils/formatHtmlParagraphs";
 
 export interface HeroCoursesProps {
-    title: string;
-    description: string;
-    updatedAt?: string;
-    img?: {
-        src: string;
-        alt?: string;
-    }
-    breadcrumbItems: {
-        label: string;
-        url: string;
-    }[]
+  title: string;
+  description: string;
+  updatedAt?: string;
+  img?: {
+    src: string;
+    alt?: string;
+  };
+  breadcrumbItems: {
+    label: string;
+    url: string;
+  }[];
 }
 
 export default function HeroCourses(args: HeroCoursesProps) {
-    return (
-        <div className="bg-primary-100 w-full py-64 flex items-center justify-center">
-            <div className="container">
-                <Breadcrumb
-                    items={args.breadcrumbItems}
-                    className="mb-64"
-                />
-                <div className="flex lg:flex-row flex-col items-center gap-64">
-                    <div className="w-full">
-                        <h1 className="text-3xl-bold text-primary-600 mb-32">
-                            {parseHtmlToParagraphs(args.title)}
-                        </h1>
+  return (
+    <div className="flex w-full items-center justify-center bg-primary-100 py-64">
+      <div className="container">
+        <Breadcrumb items={args.breadcrumbItems} className="mb-64" />
+        <div className="flex flex-col items-center gap-64 lg:flex-row">
+          <div className="w-full">
+            <h1 className="mb-32 text-3xl-bold text-primary-600">
+              {parseHtmlToParagraphs(args.title)}
+            </h1>
 
-                        <div className="text-m-regular space-y-16 w-full">
-                            {args.description.split("\n").map((paragraph, index) => (
-                                <p key={index}>{parseHtmlToParagraphs(paragraph)}</p>
-                            ))}
-                        </div>
-
-                        <div className="mt-64">
-                            Atualizado em {args.updatedAt ? dayjs(args.updatedAt).format('DD.MM.YYYY') : <span className="text-m-bold">Data de atualização indisponível</span>}
-                        </div>
-                    </div>
-                    {
-                        args.img && (<div className="w-full h-full flex items-center justify-center">
-                            <Image src={args.img?.src} alt={args.img.alt || ""} width={597} height={390} unoptimized/>
-                        </div>)
-                    }
-
-                </div>
+            <div className="w-full space-y-16">
+              {formatHtmlParagraphs(args.description, "text-m-regular")}
             </div>
-        </div>
 
-    )
+            <div className="mt-64">
+              Atualizado em{" "}
+              {args.updatedAt ? (
+                dayjs(args.updatedAt).format("DD.MM.YYYY")
+              ) : (
+                <span className="text-m-bold">Data de atualização indisponível</span>
+              )}
+            </div>
+          </div>
+          {args.img && (
+            <div className="flex h-full w-full items-center justify-center">
+              <Image
+                src={args.img?.src}
+                alt={args.img.alt || ""}
+                width={597}
+                height={390}
+                unoptimized
+              />
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
